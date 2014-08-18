637110c7676e60ef4432bb8e561cc970d33c118c
aregaz <illia.ratkevych@gmail.com>
2014-08-18 22:48:37 +0300

    Description added to Petition Level.

--- Infopulse.EDemocracy.Model/BusinessEntities/PetitionLevel.cs
+++ Infopulse.EDemocracy.Model/BusinessEntities/PetitionLevel.cs
@@ -3,6 +3,7 @@
 	public class PetitionLevel : BaseEntity
 	{
 		public string Name { get; set; }
+		public string Description { get { return this.Name; }}
 		public long Limit { get; set; }
 
 		public PetitionLevel(Model.PetitionLevel level)
