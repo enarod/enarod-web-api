e62ea98ab5fc61ec6e06204a54852c81beae478e
Illia Ratkevych <illia.ratkevych@gmail.com>
2014-12-28 23:58:44 +0200

    Petition email vote - models updated.

---
+++ Infopulse.EDemocracy.Model/ClientEntities/v2/EmailVote.cs
@@ -0,0 +1,8 @@
+﻿namespace Infopulse.EDemocracy.Model.ClientEntities.v2
+{
+	public class EmailVote
+	{
+		public long ID { get; set; }
+		public PetitionSigner Signer { get; set; }
+	}
+}
---
+++ Infopulse.EDemocracy.Model/ClientEntities/v2/PetitionSigner.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace Infopulse.EDemocracy.Model.ClientEntities.v2
+{
+	public class PetitionSigner
+	{
+		public string Email { get; set; }
+		
+		[JsonProperty("signator-name")]
+		public string FirstName { get; set; }
+
+		[JsonProperty("signator-surname")]
+		public string LastName { get; set; }
+
+		[JsonProperty("signator-middle-name")]
+		public string MiddleName { get; set; }
+
+		[JsonProperty("signator-address1")]
+		public string AddressLine1 { get; set; }
+
+		[JsonProperty("ssignator-address2")]
+		public string AddressLine2 { get; set; }
+
+		[JsonProperty("signator-city")]
+		public string City { get; set; }
+
+		[JsonProperty("signator-area")]
+		public string Region { get; set; }
+
+		[JsonProperty("signator-country")]
+		public string Country { get; set; }
+	}
+}
