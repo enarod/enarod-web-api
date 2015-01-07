5fefa8483a4c05bf003eda4ab5c0f5f1b7fea024
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-01-07 16:27:15 +0200

    GeoService test updated.

--- Infopulse.EDemocracy.Common.Tests/GeoServiceTests.cs
+++ Infopulse.EDemocracy.Common.Tests/GeoServiceTests.cs
@@ -24,5 +24,29 @@ namespace Infopulse.EDemocracy.Common.Tests
 			Assert.IsNotNull(countries);
 			Assert.IsTrue(countries.Count > 0);
 		}
+
+
+		[TestMethod]
+		public void GeoService_GetAsync_HttpServiceWorks()
+		{
+			var url = "http://api.geonames.org/countryInfo?username=aregaz&lang=uk";
+			var http = new HttpService();
+			var requestTask = http.GetAsync(url);
+			var result = requestTask.Result;
+
+			Assert.IsNotNull(result.Content);
+		}
+
+
+		[TestMethod]
+		public void GeoService_GetStringAsync_HttpServiceWorks()
+		{
+			var url = string.Format("http://api.geonames.org/countryInfo?username={0}&lang={1}", "aregaz", "uk");
+			var http = new HttpService();
+			var requestTask = http.GetStringAsync(url);
+			var result = requestTask.Result;
+
+			Assert.IsNotNull(result);
+		}
 	}
 }
