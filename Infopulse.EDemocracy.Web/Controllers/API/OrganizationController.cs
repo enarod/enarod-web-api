2c2511f105836dd2acd1f6de68b0934be001f3bf
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-02-08 18:40:32 +0200

    Get organization details - service implemented.

--- Infopulse.EDemocracy.Web/Controllers/API/OrganizationController.cs
+++ Infopulse.EDemocracy.Web/Controllers/API/OrganizationController.cs
@@ -26,6 +26,7 @@ namespace Infopulse.EDemocracy.Web.Controllers.API
 		    this.organizationRepository = new OrganizationRepository();
 	    }
 
+
 		/// <summary>
 		/// Gets all organization registered in DB.
 		/// </summary>
@@ -37,6 +38,23 @@ namespace Infopulse.EDemocracy.Web.Controllers.API
 				var organizations = this.organizationRepository.GetAll();
 				var webOrganizations = Mapper.Map<IEnumerable<Organization>>(organizations);
 				return OperationResult<IEnumerable<Organization>>.Success(webOrganizations);
+			});
+
+			return result;
+		}
+
+
+		/// <summary>
+		/// Gets all organization registered in DB.
+		/// </summary>
+		/// <returns>List of organizations.</returns>
+		public OperationResult<Organization> Get(int id)
+		{
+			var result = OperationExecuter.Execute(() =>
+			{
+				var organization = this.organizationRepository.Get(id);
+				var webOrganization = Mapper.Map<Organization>(organization);
+				return OperationResult<Organization>.Success(webOrganization);
 			});
 
 			return result;
