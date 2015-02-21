fd8c3f8f54a75192dc4016a3f4e6304c8dc15428
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-02-21 15:04:35 +0200

    Obsolete db project removed.

--- Infopulse.EDemocracy.Data.DB/dbo/Tables/Agreement.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Agreement] (
-    [ID]        BIGINT         IDENTITY (1, 1) NOT NULL,
-    [Name]      NVARCHAR (MAX) NOT NULL,
-    [ShortDesc] NVARCHAR (MAX) NOT NULL,
-    [Text]      NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_Agreement] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
--- Infopulse.EDemocracy.Data.DB/dbo/Views/vwAgreements.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE VIEW [dbo].[vwAgreements]
-	AS 
-	SELECT a.ID, a.Name, a.ShortDesc, a.Text, count(av.ID) NumberOfVotes
-	  FROM Agreement a
-	  LEFT JOIN AgreementVote av on a.ID = av.AgreementID
-	GROUP BY a.ID, a.Name, a.ShortDesc, a.Text
-
