6722bb01748e53ac16289332d2eba59a28589155
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-02-21 15:11:03 +0200

    Misspelling fix.

--- Infopulse.EDemocracy.Common/Exceptions/PetitionIsNotConfirmedException.cs
+++ Infopulse.EDemocracy.Common/Exceptions/PetitionIsNotConfirmedException.cs
@@ -3,7 +3,7 @@
 	public class PetitionIsNotConfirmedException : DomainException
 	{
 		public PetitionIsNotConfirmedException()
-			: base("Ви вже проголосували за петицію, але не підтвердили голосування через електронну пошту. Будь-ласка, перевірте вашу поштову скринську.", ExceptionLevel.BusinnessLogic)
+			: base("Ви вже проголосували за петицію, але не підтвердили голосування через електронну пошту. Будь-ласка, перевірте вашу поштову скриньку.", ExceptionLevel.BusinnessLogic)
 		{
 			
 		}
