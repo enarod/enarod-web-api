a687858f89a40aae5de0dad0ca60ca5b0d283561
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-03-12 23:05:43 +0200

    Settings table added.

---
+++ Infopulse.EDemocracy.Database/Scripts/Dictionaries.sql
@@ -0,0 +1,21 @@
+﻿;merge dbo.Settings as target
+using
+(
+	values
+	(1, 'ActivePetitionVoteCount', '10', 'Кількість голосів за петицію, яка необхідна для того, щоб петиція вважалась активною.')
+) as source (ID, [Key], [Value], [Description])
+on target.ID = source.ID
+
+when matched then
+	update
+	set
+		[Key] = source.[Key],
+		[Value] = source.[Value],
+		[Description] = source.[Description]
+
+when not matched by target then
+	insert (ID, [Key], [Value], [Description])
+	values (ID, [Key], [Value], [Description])
+	
+when not matched by source then
+	delete;
---
+++ Infopulse.EDemocracy.Database/dbo/Tables/Settings.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Settings]
+(
+	[ID] INT NOT NULL,
+	[Key] nvarchar(max) not null,
+	[Value] nvarchar(max),
+	[Description] nvarchar(max),
+	constraint PK_Settings primary key ([ID])
+)
