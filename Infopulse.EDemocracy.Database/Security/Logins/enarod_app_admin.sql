45f3a2c310fd35ae47dffdd391a5d30b1f81a57b
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-16 13:49:00 +0300

    Add DB logins to the database project. Organize files.

---
+++ Infopulse.EDemocracy.Database/Security/Logins/enarod_app_admin.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [enarod_app_admin]
+    WITH PASSWORD = N'{dxeS|UgdEnDevxTIrU|a!^umsFT7_&#$!~<ofNdcvk&p{ta', SID = 0xB78BAD4434D8A3498C178811551EB123, DEFAULT_DATABASE = [enarod_dev], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
+
---
+++ Infopulse.EDemocracy.Database/Security/Logins/enarod_app_dev.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [enarod_app_dev]
+    WITH PASSWORD = N'2{rQIdxe.|gde<vxrQoNfd?umsFT7_&#$!~<|c+v>kp{tah3', SID = 0x988B80556B5594458FFE07E1B2E89F64, DEFAULT_DATABASE = [enarod_dev], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
+
