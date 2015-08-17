ebe1cf12e4a681d6958d729e19af59f6f6ab6d62
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-18 00:29:47 +0300

    Alter app admin db role.

--- Infopulse.EDemocracy.Database/Security/RoleMemberships.sql
+++ Infopulse.EDemocracy.Database/Security/RoleMemberships.sql
@@ -4,6 +4,12 @@ ALTER ROLE [db_owner] ADD MEMBER [enarod_app_dev];
 GO
 
 
+ALTER ROLE [db_accessadmin] ADD MEMBER [enarod_app_admin];
+GO
+
+
+
+
 ALTER ROLE [db_datawriter] ADD MEMBER [enarod_app_dev];
 GO
 
