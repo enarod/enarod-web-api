cdc0b05254fd35ac3fea6ae783db6e491baddf53
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-17 23:12:29 +0300

    Update database project to use on localhost.

--- Infopulse.EDemocracy.Database/Security/RoleMemberships.sql
+++ Infopulse.EDemocracy.Database/Security/RoleMemberships.sql
@@ -1,10 +1,11 @@
 ﻿ALTER ROLE [db_owner] ADD MEMBER [enarod_app_admin];
-
-
 GO
+ALTER ROLE [db_owner] ADD MEMBER [enarod_app_dev];
+GO
+
+
 ALTER ROLE [db_datawriter] ADD MEMBER [enarod_app_dev];
-
-
 GO
-ALTER ROLE [db_datareader] ADD MEMBER [enarod_app_dev];
 
+ALTER ROLE [db_datareader] ADD MEMBER [enarod_app_dev];
+GO
