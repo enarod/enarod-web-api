e3f0ca36b13c0ea06f6373869eb7d422c6b94edb
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-18 22:32:07 +0300

    Change reference of column Petition.CreatedBy to auth.App_User.

---
+++ Infopulse.EDemocracy.Database/Scripts/Misc/MIgration_Petition.CreatedBy to auth.App_User.sql
@@ -0,0 +1,8 @@
+﻿alter table dbo.Petition
+drop FK_Petition_People
+
+alter table dbo.Petition
+alter column CreatedBy int not null
+
+alter table dbo.Petition
+add constraint FK_Petition_CreatedBy foreign key (CreatedBy) references auth.App_User
