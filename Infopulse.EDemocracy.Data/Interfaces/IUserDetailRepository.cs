13188588bc80b8956329a89d9a477f87db5be7e8
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-18 22:53:46 +0300

    intermediate commit.

--- Infopulse.EDemocracy.Data/Interfaces/IUserDetailRepository.cs
+++ Infopulse.EDemocracy.Data/Interfaces/IUserDetailRepository.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Infopulse.EDemocracy.Data.Interfaces
+﻿namespace Infopulse.EDemocracy.Data.Interfaces
 {
 	public interface IUserDetailRepository
 	{
-		
+		int GetUserId(string userEmail);
 	}
 }
--- Infopulse.EDemocracy.Data/Repositories/UserDetailRepository.cs
+++ Infopulse.EDemocracy.Data/Repositories/UserDetailRepository.cs
@@ -4,5 +4,12 @@ namespace Infopulse.EDemocracy.Data.Repositories
 {
 	public class UserDetailRepository : IUserDetailRepository
 	{
-	}
+		public int GetUserId(string userEmail)
+		{
+			//using (var db = new AuthContext())
+			//{
+			//}
+			return -1;
+		}
+    }
 }
