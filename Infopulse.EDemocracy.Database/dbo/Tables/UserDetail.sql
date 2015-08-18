ed0153fc5633b301dea78e97a44cc02fa47b2534
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-18 22:01:39 +0300

    Add UserDetail table to the DB project. Add auth.App_User table for consistency.

---
+++ Infopulse.EDemocracy.Database/dbo/Tables/UserDetail.sql
@@ -0,0 +1,20 @@
+﻿create table dbo.UserDetail
+(
+	ID int not null identity(1, 1),
+	UserID int not null,
+	FirstName nvarchar(300),
+	MiddleName nvarchar(300),
+	LastName nvarchar(300),
+	ZipCode nvarchar(10) default '00000',
+	AddressLine1 nvarchar(4000),
+	AddressLine2 nvarchar(4000),
+	City nvarchar(300),
+	Region nvarchar(300),
+	Country nvarchar(300),
+	CreatedBy nvarchar(4000) not null default 'Unknown DB user',
+	CreatedDate datetime2 not null default getutcdate(),
+	ModifiedBy nvarchar(4000),
+	ModifiedDate datetime2,
+	constraint PK_UserDetail primary key (ID),
+	constraint FK_UserDetail_Auth_App_User foreign key (UserID) references auth.App_User(ID)
+)
