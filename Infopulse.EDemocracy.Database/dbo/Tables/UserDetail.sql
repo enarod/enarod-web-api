69b6299a261bf1ba6a4fbb73148eb875ebb69289
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-18 22:17:18 +0300

    Add unique constraint on UserDetail.UserId column.

--- Infopulse.EDemocracy.Database/dbo/Tables/UserDetail.sql
+++ Infopulse.EDemocracy.Database/dbo/Tables/UserDetail.sql
@@ -1,20 +1,21 @@
-﻿create table dbo.UserDetail
-(
-	ID int not null identity(1, 1),
-	UserID int not null,
-	FirstName nvarchar(300),
-	MiddleName nvarchar(300),
-	LastName nvarchar(300),
-	ZipCode nvarchar(10) default '00000',
-	AddressLine1 nvarchar(4000),
-	AddressLine2 nvarchar(4000),
-	City nvarchar(300),
-	Region nvarchar(300),
-	Country nvarchar(300),
-	CreatedBy nvarchar(4000) not null default 'Unknown DB user',
-	CreatedDate datetime2 not null default getutcdate(),
-	ModifiedBy nvarchar(4000),
-	ModifiedDate datetime2,
-	constraint PK_UserDetail primary key (ID),
-	constraint FK_UserDetail_Auth_App_User foreign key (UserID) references auth.App_User(ID)
-)
+﻿CREATE TABLE [dbo].[UserDetail] (
+    [ID]           INT             IDENTITY (1, 1) NOT NULL,
+    [UserID]       INT             NOT NULL,
+    [FirstName]    NVARCHAR (300)  NULL,
+    [MiddleName]   NVARCHAR (300)  NULL,
+    [LastName]     NVARCHAR (300)  NULL,
+    [ZipCode]      NVARCHAR (10)   DEFAULT ('00000') NULL,
+    [AddressLine1] NVARCHAR (4000) NULL,
+    [AddressLine2] NVARCHAR (4000) NULL,
+    [City]         NVARCHAR (300)  NULL,
+    [Region]       NVARCHAR (300)  NULL,
+    [Country]      NVARCHAR (300)  NULL,
+    [CreatedBy]    NVARCHAR (4000) DEFAULT ('Unknown DB user') NOT NULL,
+    [CreatedDate]  DATETIME2 (7)   DEFAULT (getutcdate()) NOT NULL,
+    [ModifiedBy]   NVARCHAR (4000) NULL,
+    [ModifiedDate] DATETIME2 (7)   NULL,
+    CONSTRAINT [PK_UserDetail] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_UserDetail_Auth_App_User] FOREIGN KEY ([UserID]) REFERENCES [auth].[App_User] ([Id]),
+    CONSTRAINT [UQ_UserId] UNIQUE NONCLUSTERED ([UserID] ASC)
+);
+
