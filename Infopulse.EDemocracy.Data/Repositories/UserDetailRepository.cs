11e77c56936e6e1ba99ec8cd2c4f1d33692c86c0
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-08-19 00:11:13 +0300

    Implement repo for getting UserID by user email.

--- Infopulse.EDemocracy.Data/Repositories/UserDetailRepository.cs
+++ Infopulse.EDemocracy.Data/Repositories/UserDetailRepository.cs
@@ -1,4 +1,9 @@
 ﻿using Infopulse.EDemocracy.Data.Interfaces;
+using Infopulse.EDemocracy.Model;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace Infopulse.EDemocracy.Data.Repositories
 {
@@ -6,10 +11,20 @@ namespace Infopulse.EDemocracy.Data.Repositories
 	{
 		public int GetUserId(string userEmail)
 		{
-			//using (var db = new AuthContext())
-			//{
-			//}
-			return -1;
+			using (var db = new EDEntities())
+			{
+				var userID = db.Database.SqlQuery<int>(
+					"sp_User_GetIdByEmail @UserEmail",
+					new SqlParameter()
+					{
+						ParameterName = "UserEmail",
+						DbType = DbType.String,
+						Value = userEmail,
+						Direction = ParameterDirection.Input
+					});
+
+				return userID.SingleOrDefault();
+			}
 		}
     }
 }
