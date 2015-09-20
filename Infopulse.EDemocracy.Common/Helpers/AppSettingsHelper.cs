b4a4ddd718e275bbd0f493b2e729d1d7a3929205
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-09-20 21:57:25 +0300

    Add AppSettingHelper.

---
+++ Infopulse.EDemocracy.Common/Helpers/AppSettingsHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace Infopulse.EDemocracy.Common.Helpers
+{
+	public static class AppSettingsHelper
+	{
+		private static string domain;
+		private static string Domain
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(domain))
+				{
+					AppSettingsHelper.domain = ConfigurationManager.AppSettings["AppDomain"];
+				}
+
+				return AppSettingsHelper.domain;
+			}
+
+		}
+	}
+}
