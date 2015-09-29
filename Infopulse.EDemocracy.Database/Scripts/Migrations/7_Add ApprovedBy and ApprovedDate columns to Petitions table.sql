717352d15f03fb030dd8dbd42d6a900a72a8f647
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-09-29 22:11:30 +0300

    Add migration script.

---
+++ Infopulse.EDemocracy.Database/Scripts/Migrations/7_Add ApprovedBy and ApprovedDate columns to Petitions table.sql
@@ -0,0 +1,13 @@
+﻿alter table dbo.Petition
+add [ApprovedBy]       INT            NULL
+
+go
+
+alter table dbo.Petition
+add [ApprovedDate]     DATETIME       NULL
+
+go
+
+alter table dbo.Petition
+add constraint [FK_Petition_ApprovedBy] foreign key ([ApprovedBy]) references [auth].[App_User] ([Id])
+go
