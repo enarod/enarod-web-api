86a13302fb2d3ff4bbbdf22f04f79922b39d0950
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-10-15 00:15:30 +0300

    Add Utils js helper.

---
+++ Infopulse.EDemocracy.Web/Areas/Admin/app/utils.js
@@ -0,0 +1,15 @@
+﻿var Utils = (function() {
+	var tokenKey = 'accessToken';
+
+	function getAuthorizationHeader() {
+		if (localStorage.getItem(tokenKey) === null) {
+			return null;
+		} else {
+			return 'Bearer ' + localStorage.getItem(tokenKey);
+		}
+	};
+
+	return {
+		GetAuthorizationHeader: getAuthorizationHeader
+	};
+})();
