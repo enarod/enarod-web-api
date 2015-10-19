a52eedee73f32e5ce7823fc4ff67858216ef5eab
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-10-20 01:08:57 +0300

    Move ModerationPetition to the Model project.

---
+++ Infopulse.EDemocracy.Model/BusinessEntities/Admin/ModeratedPetition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Infopulse.EDemocracy.Model.BusinessEntities.Admin
+{
+	/// <summary>
+	/// Petition with cpecial properties indicating moderation status.
+	/// </summary>
+	public class ModeratedPetition : Petition
+	{
+		/// <summary>
+		/// Indicates whether moderator checked current petition for specific action.
+		/// </summary>
+		public bool IsChecked { get; set; }
+
+		/// <summary>
+		/// Moderator info.
+		/// </summary>
+		public UserDetailInfo Approver { get; set; }
+
+		/// <summary>
+		/// Date of petition approvement.
+		/// </summary>
+		public DateTime? ApprovedDate { get; set; }
+    }
+}
