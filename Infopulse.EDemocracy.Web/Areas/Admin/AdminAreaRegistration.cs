54e0962766720d9670a3235bd23364c5ffcbb16b
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-11-03 23:34:44 +0200

    Code clean-up.

--- Infopulse.EDemocracy.Web/Areas/Admin/AdminAreaRegistration.cs
+++ Infopulse.EDemocracy.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -5,15 +5,9 @@ namespace Infopulse.EDemocracy.Web.Areas.Admin
 {
     public class AdminAreaRegistration : AreaRegistration 
     {
-        public override string AreaName 
-        {
-            get 
-            {
-                return "Admin";
-            }
-        }
+        public override string AreaName => "Admin";
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+	    public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
                 name: "Admin_default",
