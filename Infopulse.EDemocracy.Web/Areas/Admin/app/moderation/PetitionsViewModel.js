5cbec2d56533ceba1de52fee353fe598edcdaae7
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-11-03 21:13:32 +0200

    JS refactoring.

--- Infopulse.EDemocracy.Web/Areas/Admin/app/moderation/PetitionsViewModel.js
+++ Infopulse.EDemocracy.Web/Areas/Admin/app/moderation/PetitionsViewModel.js
@@ -2,11 +2,17 @@
 	var self = this;
 
 	self.init = function() {
-		
+		ko.postbox.subscribe('signedIn', function(e) {
+			self.getPetitions();
+		});
 	};
 
 	self.petitions = ko.observableArray();
 	self.selectedPetitions = ko.observableArray();
+
+	self.buttonIsEnabled = ko.computed(function () {
+		return self.selectedPetitions.length > 0;
+	});
 
 	self.getPetitions = function () {
 		var authorizationHeader = Utils.GetAuthorizationHeader();
@@ -21,25 +27,27 @@
 				}
 			})
 			.done(function (responseData) {
-					for (var petitionIndex in responseData.Data) {
-						if (responseData.Data.hasOwnProperty(petitionIndex)) {
-							var petition = responseData.Data[petitionIndex];
-							petition.ApproverEmail = (petition.Approver && petition.Approver.User)
-								? petition.Approver.User.Email
-								: '';
-							petition.ApprovedDate = petition.ApprovedDate == null
-								? ''
-								: moment(petition.ApprovedDate).format('DD.MM.YYYY hh:mm');
-							if (petition.PetitionStatus.ID === 1 && petition.Approver != null) {
-								petition.AdminStatus = 'На модерації';
-							} else if (petition.PetitionStatus.ID === 1 && petition.Approver == null) {
-								petition.AdminStatus = 'Створена';
-							} else {
-								petition.AdminStatus = petition.PetitionStatus.Name;
-							}
+				for (var petitionIndex in responseData.Data) {
+					if (responseData.Data.hasOwnProperty(petitionIndex)) {
+						var petition = responseData.Data[petitionIndex];
+						petition.ApproverEmail = (petition.Approver && petition.Approver.User)
+							? petition.Approver.User.Email
+							: '';
+						petition.ApprovedDate = petition.ApprovedDate == null
+							? ''
+							: moment(petition.ApprovedDate).format('DD.MM.YYYY hh:mm');
+						if (petition.PetitionStatus.ID === 1 && petition.Approver != null) {
+							petition.AdminStatus = 'На модерації';
+						} else if (petition.PetitionStatus.ID === 1 && petition.Approver == null) {
+							petition.AdminStatus = 'Створена';
+						} else {
+							petition.AdminStatus = petition.PetitionStatus.Name;
 						}
 					}
-					self.petitions(responseData.Data);
+				}
+				self.petitions(responseData.Data);
+
+				var grid = new Slick.Grid("#petitionsGrid", self.petitions(), columns, options);
 			})
 			.error(function (jqXHR, textStatus, errorThrown) {
 				console.log("Error: " + errorThrown);
@@ -90,25 +98,25 @@
 		})
 		.error(function (jqXHR, textStatus, errorThrown) {
 			console.log("Error: " + errorThrown);
-			
+
 			// if error code is 401 Unauthorized then show login window
 			switch (errorThrown) {
 				case "Unauthorized":
-				{
-					alert('Incorrect access token. Please re-signin.');
-					ko.postbox.publish('signInRequested');
-					break;
-				}
+					{
+						alert('Incorrect access token. Please re-signin.');
+						ko.postbox.publish('signInRequested');
+						break;
+					}
 				case "Internal Server Error":
-				{
-					alert(jqXHR.responseJSON.ExceptionMessage);
-					break;
-				}
+					{
+						alert(jqXHR.responseJSON.ExceptionMessage);
+						break;
+					}
 				default:
-				{
-					console.log(jqXHR);
-					break;
-				}
+					{
+						console.log(jqXHR);
+						break;
+					}
 			}
 		});
 	};
